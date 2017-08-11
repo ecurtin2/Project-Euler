b4e4b09d944be851147587462dc2880dc4fb4571
Evan <ecurtin@illinois.edu>
2017-08-11 15:09:44 -0500

    Problem 3

---
+++ src/Project-Euler-003.py
@@ -0,0 +1,40 @@
+import argparse
+import functools
+import itertools
+
+@functools.lru_cache()
+def factorize(n):
+    if n < 0:
+        yield -1
+        n *= -1
+    elif n > 2:
+        i = 2
+        while n % i != 0 and i <= n:
+            i += 1
+                
+        yield i
+        yield from factorize(n // i)        
+    else:
+        yield n
+    
+        
+ 
+
+def main():
+    p = argparse.ArgumentParser(
+        description="Solve Project Euler Problem 2 for input N.")
+    p.add_argument('--N', type=int, default=600851475143,
+        help='The upper bound (inclusive) of the fibonacci sequence considered.')
+    
+    variables = vars(p.parse_args())
+
+    N = variables['N']
+
+    factors = list(factorize(N))
+    max_factor = max(factors)
+    print("The highest prime factor of {N} is {val}".format(N=N, val=max_factor))
+    print("All prime factors are {}.".format(factors))
+    
+
+if __name__ == "__main__":
+    main()
